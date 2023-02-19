6bd9da7b6e256c43d9fb4e7503adcbe9c62cbda3
Kate <cuttysark1010@gmail.com>
2023-02-19 19:47:08 +0300

    change calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,35 +1,25 @@
-import readlineSync from 'readline-sync';
+import gameLogic from '../index.js';
 
-const calc = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('What is the result of the expression?');
-  for (let i = 1; i <= 3; i += 1) {
-    const randomNumber1 = Math.floor(Math.random() * 10);
-    const randomNumber2 = Math.floor(Math.random() * 10);
-    const operations = ['+', '-', '*'];
-    const random = Math.floor(Math.random() * operations.length);
-    const randomOperation = operations[random];
-    console.log(`Question: ${randomNumber1} ${randomOperation} ${randomNumber2}`);
-    const userAnswer = Number(readlineSync.question('Your answer: '));
-    let correctAnswer;
-    switch (randomOperation) {
-      case '+': correctAnswer = randomNumber1 + randomNumber2;
-        break;
-      case '-': correctAnswer = randomNumber1 - randomNumber2;
-        break;
-      default: correctAnswer = randomNumber1 * randomNumber2;
-    }
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-      return;
-    }
+const generateRound = () => {
+  const randomNumber1 = Math.floor(Math.random() * 10);
+  const randomNumber2 = Math.floor(Math.random() * 10);
+  const operations = ['+', '-', '*'];
+  const random = Math.floor(Math.random() * operations.length);
+  const randomOperation = operations[random];
+  const question = `${randomNumber1} ${randomOperation} ${randomNumber2}`;
+  let answer;
+  switch (randomOperation) {
+    case '+': answer = randomNumber1 + randomNumber2;
+      break;
+    case '-': answer = randomNumber1 - randomNumber2;
+      break;
+    default: answer = randomNumber1 * randomNumber2;
   }
-  console.log(`Congratulations, ${userName}!`);
+  const correctAnswer = String(answer);
+  return [question, correctAnswer];
 };
 
-export default calc;
+const calcGame = () => {
+  gameLogic('What is the result of the expression?', generateRound);
+};
+export default calcGame;
