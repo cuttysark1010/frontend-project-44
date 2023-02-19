463f81498cbcf09ef483bc15f6c87741ed3df411
Kate <cuttysark1010@gmail.com>
2023-02-19 19:50:22 +0300

    added unified game logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const gameLogic = (rules, round) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
+  for (let i = 1; i <= 3; i += 1) {
+    const [question, correctAnswer] = round();
+    console.log(`Question: ${[question]}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
-export default greeting;
+export default gameLogic;
