83ccca7a12b9e61821dd623e021a321d29a14b7e
Kate <cuttysark1010@gmail.com>
2023-02-28 15:39:05 +0300

    added edits version 5

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,20 +1,25 @@
 import runLogic from '../index.js';
 import { makeRandomNum100 } from '../utils.js';
 
-const generateRound = () => {
-  const randomNumber1 = makeRandomNum100();
-  const randomNumber2 = makeRandomNum100();
-  const question = `${randomNumber1} ${randomNumber2}`;
-  const intermediateResult = [];
+const isMultiple = (num1, num2) => {
+  const result = [];
   let i = 1;
-  while (i <= randomNumber1) {
-    if ((randomNumber1 % i === 0) && (randomNumber2 % i === 0)) {
-      intermediateResult.push(i);
+  while (i <= num1) {
+    if ((num1 % i === 0) && (num2 % i === 0)) {
+      result.push(i);
       i += 1;
     } else {
       i += 1;
     }
   }
+  return result;
+};
+
+const generateRound = () => {
+  const randomNumber1 = makeRandomNum100();
+  const randomNumber2 = makeRandomNum100();
+  const question = `${randomNumber1} ${randomNumber2}`;
+  const intermediateResult = isMultiple(randomNumber1, randomNumber2);
   const answer = intermediateResult[intermediateResult.length - 1];
   const correctAnswer = String(answer);
   return [question, correctAnswer];
